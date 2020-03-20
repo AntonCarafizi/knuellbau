9cc9454515eb62a853d0c077f44fe5729485ad83
Anton Carafizi <anton.carafizi@gmail.com>
2020-03-20 14:21:11 +0100

    Modify 404.php

--- themes/knuellbau/404.php
+++ themes/knuellbau/404.php
@@ -1,12 +1,16 @@
 <?php get_header(); ?>
-<main class="content_box">
-<div class="container">
-<div class="jumbotron w-100 bg-light text-center">
-  <h1 class="display-4"><?php _e( 'Page not found' ); ?></h1>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg red" href="<?php echo home_url(); ?>" role="button" title="<?php _e( 'Go to Homepage' ); ?>"><?php _e( 'Go to Homepage' ); ?></a>
-  </p>
-</div>
-</div>
+<main role="main">
+    <div class="container p-0 p-md-5 p-lg-5 p-xl-5 bg-transparent">
+        <div class="container p-3 p-md-5 p-lg-5 p-xl-5 bg-white">
+            <div class="jumbotron w-100 bg-light text-center">
+                <h1 class="display-3"><?php _e('Error'); ?> 404</h1>
+                <h2 class="display-4"><?php _e('Page not found'); ?></h2>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg red" href="<?php echo home_url(); ?>" role="button"
+                       title="<?php _e('Go to Homepage'); ?>"><?php _e('Go to Homepage'); ?></a>
+                </p>
+            </div>
+        </div>
+    </div>
 </main>
 <?php get_footer(); ?>
